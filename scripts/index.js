b6299ce91c2c164e80ca21300e8ad67a8345a3b0
Richard King <richrdkng@gmail.com>
2020-03-28 19:50:43 +0100

    ci: fix NPM release issue

--- scripts/index.js
+++ scripts/index.js
@@ -8,21 +8,24 @@ const debug = require('gulp-debug')
 
 const root = path.resolve(__dirname, '../')
 
-gulp.task('build:clean', async () => {
+gulp.task('pre-build', async () => {
   del(`${root}/dist`, { force: true })
+
   await delay(500)
+
+  gulp
+    .src(`${root}/package.json`)
+    .pipe(debug())
+    .pipe(gulp.dest(`${root}/dist`))
 })
 
-gulp.task('build:copy', async () => {
+gulp.task('build', async () => {
   gulp
     .src([
       `${root}/README*`,
       `${root}/LICENSE*`,
-      `${root}/package.json`,
       `${root}/src/**/*.js`
     ])
     .pipe(debug())
     .pipe(gulp.dest(`${root}/dist`))
 })
-
-gulp.task('build', gulp.series('build:clean', 'build:copy'))
