2c31b4ce574acc6c39ef83d9b1d4dc30e7165513
Richard King <richrdkng@gmail.com>
2020-04-17 22:17:49 +0200

    build: simplify pre-build script and release config

--- scripts/index.js
+++ scripts/index.js
@@ -2,22 +2,9 @@
 
 const path = require('path')
 const gulp = require('gulp')
-const del = require('del')
-const delay = require('delay')
 const debug = require('gulp-debug')
 
 const root = path.resolve(__dirname, '../')
-
-gulp.task('pre-build', async () => {
-  del(`${root}/dist`, { force: true })
-
-  await delay(500)
-
-  gulp
-    .src(`${root}/package.json`)
-    .pipe(debug())
-    .pipe(gulp.dest(`${root}/dist`))
-})
 
 gulp.task('build', async () => {
   gulp
